0cc41a55a75dd0a92a30a88d42fb58320dc7a417
NathanTan <tann@oregonstate.edu>
2018-07-14 19:54:31 -0700

    Nothing much.

--- ChessState.js
+++ ChessState.js
@@ -1,4 +1,18 @@
 import boardPrinter from './boardPrinter'
 import constants from './constants'
 
+class ChessState {
+    constructor() {
+
+        this.board = constants.startingFen
+    }
+
+
+
+}
+
+
+let state = new ChessState()
+
 console.log("The start")
+console.log(state.board)
