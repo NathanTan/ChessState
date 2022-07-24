b8a74ab990cca5edf60229da5f7065a000049fa8
NathanTan <nathan1011@protonmail.com>
2022-07-24 11:19:01 -0700

    Fix webpack to work npx webpack --mode evelopment

--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,13 @@ const outputFolder = debug ? "/bundles" : "/dist"
 
 module.exports = {
   context: __dirname,
-  devtool: debug ? "inline-sourcemap" : null,
+  devtool: debug ? 'inline-source-map',
   
   entry: "./src/ChessState.ts",
   module: {
      rules: [
      {
-        test: "/\.ts?$",
+        test: /\.ts$/, 
         use: 'ts-loader',
         exclude: /node_modules/
      }
